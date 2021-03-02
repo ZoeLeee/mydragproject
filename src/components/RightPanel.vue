09469bb8f8bfc41019e7185d8a1325df4ea078d8
Zoe <lizhou1021@qq.com>
2021-03-02 21:03:13 +0800

    UI样式

---
+++ src/components/RightPanel.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="right">
+    <Tabs>
+      <TabPane label="属性" name="style"> 属性 </TabPane>
+      <TabPane label="动画" name="animate"> 动画 </TabPane>
+    </Tabs>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { Tabs, TabPane } from "element-ui";
+
+@Component({
+  components: {
+    Tabs,
+    TabPane,
+  },
+})
+export default class RightPanel extends Vue {
+  @Prop() private msg!: string;
+  data() {
+    return {
+      activeName: "style",
+    };
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,101 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Container direction="vertical">
+      <Header>
+        <Button>撤销</Button>
+        <Button>重做</Button>
+        <div class="size">
+          <span>画布大小</span>
+          <input type="text" v-model="width" />*
+          <input type="text" v-model="height" />
+        </div>
+      </Header>
+      <Container>
+        <Aside width="300px">Aside</Aside>
+        <Main>
+          <div class="content">
+            <div
+              class="editor"
+              v-bind:style="{ width: width + 'px', height: height + 'px' }"
+            >
+              画布
+            </div>
+          </div>
+        </Main>
+        <Aside width="300px">
+          <RightPanel />
+        </Aside>
+      </Container>
+    </Container>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Vue } from "vue-property-decorator";
+import RightPanel from "@/components/RightPanel.vue"; // @ is an alias to /src
+import { Container, Header, Aside, Main, Button, Input } from "element-ui";
 
 @Component({
   components: {
-    HelloWorld,
+    Container,
+    Header,
+    Aside,
+    Main,
+    Button,
+    Input,
+    RightPanel,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  data() {
+    return {
+      width: 1200,
+      height: 600,
+    };
+  }
+}
 </script>
+
+<style lang="less">
+.home {
+  height: 100%;
+  & > .el-container {
+    height: 100%;
+    .el-container {
+      margin-top: 1px;
+    }
+  }
+  .el-main {
+    background: #f5f5f5;
+    padding: 2%;
+    position: relative;
+    overflow: hidden;
+  }
+  .el-header {
+    outline: 1px solid #ccc;
+    display: flex;
+    align-items: center;
+  }
+  .el-aside {
+    padding: 20px;
+  }
+  .size {
+    margin-left: 20px;
+    display: flex;
+    align-items: center;
+    input {
+      height: 20px;
+      width: 50px;
+    }
+  }
+  .content {
+    background: #fff;
+    overflow: auto;
+    height: 100%;
+    .editor {
+      margin: auto;
+      position: relative;
+    }
+  }
+}
+</style>
